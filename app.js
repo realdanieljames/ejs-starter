3269d4c8be403a7e966e48e60eff1532ab2d9318
DANIEL JAMES <daniel.james@codeimmersives.com>
2020-10-19 23:23:52 -0400

    updated files and folders

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
 
+
+const path = require('path')
+
+// set the view for express
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+//use static assets
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 app.use('/ejs', require('./routes/ejsRoutes'));
 const port = 3000;
 app.listen(port, () => {
--- routes/ejsRoutes.js
+++ routes/ejsRoutes.js
@@ -1,7 +1,59 @@
 const router = require('express').Router();
+const axios = require('axios');
+//====================================================================================//
+//====================================================================================//
 
-router.get('/', (req, res) => {
-  res.send('Now its time for the view');
+
+
+router.get('/intro', (req, res) => {
+    //   res.send('Now its time for the view');
+
+    return res.render('main/intro', { myName: 'Daniel' });
 });
+//====================================================================================//
+//====================================================================================//
+
+router.get('/p/:dog/:cat', (req, res) => {
+    let car = req.query.search;
+    let dog = req.params.dog;
+    let cat = req.params.cat;
+    return res.render('main/params', { myDog: dog, myCat: cat, myCar: car });
+});
+//====================================================================================//
+//====================================================================================//
+
+
+router.get('/loop', (req, res) => {
+    let places = [
+        { city: 'Stamford', state: 'CT' },
+        { city: 'New York', state: 'NY' },
+        { city: 'Danbury', state: 'CT' }
+    ];
+    return res.render('main/index', { myPlaces: places });
+});
+//====================================================================================//
+//====================================================================================//
+
+
+
+router.get('/about', (req, res) => {
+    return res.render('main/about');
+});
+//====================================================================================//
+//====================================================================================//
+
+
+router.get('/url', (req, res) => {
+    axios
+        .get('https://www.anapioficeandfire.com/api/books?pageSize=10')
+        .then((results) => {
+            // console.log(Array.isArray(results.data));
+            let myData = results.data;
+            return res.render('main/url', { myData: myData });
+        });
+});
+//====================================================================================//
+//====================================================================================//
+
 
 module.exports = router;
